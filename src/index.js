9efc48d75d1680767fb6695f8efbebc7b3b896dc
ViktoriaDem <demidovich.viktoria@gmail.com>
2023-08-13 20:37:51 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let numbers = Math.abs(n).toString().split('');
+  for(let i = numbers.length - 1; i >= 0; i--) {
+    result += numbers[i];
+  }
+  return Number(result);
 }
